de3094f6e938e7d324603c112ab4ad271d1028e2
Brad Conklin <brad@otronline.com>
2024-03-25 11:54:28 -0600

    secure env variables

--- backend/db.js
+++ backend/db.js
@@ -9,11 +9,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
-console.log({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
